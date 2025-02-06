215a70713040f6dfdff87c34d118f9099e041a38
ECED3901 8 <cassie@dal.ca>
2025-02-06 09:29:53 -0400

    Changes to C code for ADC reading setup on R Pi Pico

---
+++ build/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/home/student/eced3901-serialsensor-base/build/_deps/picotool/"
+};
--- main.c
+++ main.c
@@ -2,12 +2,25 @@
 
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
 
 #define PIN_LED PICO_DEFAULT_LED_PIN
 
 int main() {
     stdio_init_all();
     stdio_set_translate_crlf(&stdio_usb, false);
+    //initialize the ADC module:
+    printf("ADC Example, measuring GPIO26\n");
+    adc_init();
+    adc_gpio_init(26);
+    adc_select_input(0);
+    //read and adc value
+    const float conversion_factor = 3.3f / (1<<12);
+
+
+  
+
 
     //WARNING: Default library needs flow control set to RTS/CTS - otherwise port will
     // not connect. The LED turns on once port is detected in firmware.
@@ -16,10 +29,10 @@ int main() {
     gpio_init(PIN_LED);
     gpio_set_dir(PIN_LED, GPIO_OUT);
     gpio_put(PIN_LED, 1);
-
-    int adc = 22;
+    
 
     while(1){
+        uint16_t result= adc_read();
         char command = getchar();
 
         switch(command){
@@ -33,7 +46,7 @@ int main() {
                 gpio_put(PIN_LED, 1);
                 break;
             case 'S':
-                printf("ADC: %d\n", adc);
+                printf("ADC: %d\n", result);
                 break;
 
             default:
